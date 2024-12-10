973038581d0304bf6554b5341e8e35eca40858fb
jalil.chaibderraine <jalil.chaibderraine@etu.umontpellier.fr>
2024-12-10 11:53:35 +0100

    test

--- src/login/bl/User.java
+++ src/login/bl/User.java
@@ -9,9 +9,8 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private Role role;
 
-<<<<<<< HEAD
-    // Getters and setters
     public String getUsername() {
         return this.username;
     }
@@ -40,140 +39,9 @@ public class User {
     public User() {}
 
     // Constructor with username and password
-    public User(String username, String password) {
+    public User(String username, String password , Role r) {
         this.username = username;
         this.password = password;
+        this.role=r
     }
 }
-=======
-    /**
-     * Default constructor
-     */
-    public User() {
-    }
-
-    /**
-     * 
-     */
-    private string username;
-
-    /**
-     * 
-     */
-    private string password;
-
-    /**
-     * 
-     */
-    private string name;
-
-    /**
-     * 
-     */
-    private string firstname;
-
-    /**
-     * 
-     */
-    private string email;
-
-    /**
-     * 
-     */
-    private Role role;
-
-    /**
-     * @return
-     */
-    public string getUsername() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public string getPassword() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public string getName() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public string getFirstName() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public string getEmail() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public Role getRole() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param username 
-     * @return
-     */
-    public void setUsername(void username) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param password 
-     * @return
-     */
-    public void setPassword(void password) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param name 
-     * @return
-     */
-    public void setName(void name) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param firstname 
-     * @return
-     */
-    public void setFirstName(void firstname) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param email 
-     * @return
-     */
-    public void setEmail(void email) {
-        // TODO implement here
-        return null;
-    }
-
-}
->>>>>>> 10effe60dafa487a1b0a800717ff73114fd2c7aa
--- src/login/dao/UserDaoMySql.java
+++ src/login/dao/UserDaoMySql.java
@@ -9,17 +9,13 @@ import java.util.*;
  */
 public class UserDAOMySQL implements UserDAO, UserDAO {
 
-    /**
-     * Default constructor
-     */
+    
+
     public UserDAOMySQL() {
     }
 
-    /**
-     * 
-     */
     public void saveUser() {
-        // TODO implement here
+
     }
 
     /**
@@ -29,19 +25,6 @@ public class UserDAOMySQL implements UserDAO, UserDAO {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
-    public void saveUser() {
-        // TODO implement UserDAO.saveUser() here
-    }
-
-    /**
-     * 
-     */
-    public void loadUser() {
-        // TODO implement UserDAO.loadUser() here
-    }
 
 }
->>>>>>> 10effe60dafa487a1b0a800717ff73114fd2c7aa
+
